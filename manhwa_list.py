fa34e14d39d30406f5d1470f4c6c986064749e92
ShinHo Chul <dsp@ShinHoui-MacBook-Pro.local>
2016-07-03 00:19:02 +0900

    만화리스트 업데이트

--- manhwa_list.py
+++ manhwa_list.py
@@ -11,11 +11,15 @@ lists = {
     "베르세르크":"http://onenable.net/bbs/board.php?bo_table=toonia56",
     "블리치":"http://onenable.net/bbs/board.php?bo_table=toonia26",
     "베가본드":"http://onenable.net/bbs/board.php?bo_table=toonia32",
+    "아인":"http://onenable.net/bbs/board.php?bo_table=toonia963"
     #네이버 만화
     "고수":"http://comic.naver.com/webtoon/list.nhn?titleId=662774",
     "신의언어":"http://comic.naver.com/webtoon/list.nhn?titleId=602287",
     "덴마":"http://comic.naver.com/webtoon/list.nhn?titleId=119874",
     "신의탑":"http://comic.naver.com/webtoon/list.nhn?titleId=183559",
     "아일랜드1부":"http://comic.naver.com/webtoon/list.nhn?titleId=678500",
-    "심심한마왕":"http://comic.naver.com/webtoon/list.nhn?titleId=374974"
+    "심심한마왕":"http://comic.naver.com/webtoon/list.nhn?titleId=374974",
+    "스퍼맨":"http://comic.naver.com/webtoon/list.nhn?titleId=674209",
+    "한번더해요":"http://comic.naver.com/webtoon/list.nhn?titleId=675393",
+    "노블레스":"http://comic.naver.com/webtoon/list.nhn?titleId=25455"
 }
